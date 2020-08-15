4edc0ac26e5999f14b3e2906c8c5a680ab4d85c3
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-08-15 11:38:29 +0900

    topicの修正

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,8 +13,8 @@ class TopicsController < ApplicationController
   @tribune = page3.search('//p[contains(@class, "article-title")]')
 
   agent4 = Mechanize.new
-  page4 = agent4.get("https://diamond.jp/category/dol")
-  @d = page4.search('//article/a')
+  page4 = agent4.get("https://www.carenet.com/")
+  @carenet = page4.search('//div[contains(@class, "post-ranking__text-item")]')
 
   agent5 = Mechanize.new
   page5 = agent5.get("http://www.dhbr.net/")
